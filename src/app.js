951835596a272fdafb54cac337a543f2e255b113
DiegoKoyama <yukioko.jp@gmail.com>
2023-06-09 15:24:36 -0300

    alteracao para deploy

--- src/app.js
+++ src/app.js
@@ -8,5 +8,5 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Rodando servidor na porta ${PORT}`));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Rodando servidor na porta ${port}`));
